e6cb01ea6ecd0d893a6f64eab069657b760c990c
Luc Abayo <luc.bayo@gmail.com>
2020-02-24 11:40:04 +0200

    :sparkles: quiz screen (#6)
    - Adds home header navigation
    - Adds quiz screen: title, questions, answers, and next button
    - Add chat header title
    
    Luc :green_heart:

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { responsiveHeight, responsiveWidth } from '../../utils/responsivePixel';
 export default StyleSheet.create({
 	container: {
 		flex: 1
@@ -12,5 +13,33 @@ export default StyleSheet.create({
 	title: {
 		fontSize: 18,
 		fontWeight: 'bold'
+	},
+	headerNavigation: {
+		flexDirection: 'row',
+		backgroundColor: 'rgba(0,0,0,0.3)',
+		justifyContent: 'space-between'
+	},
+	navigationButton: {
+		width: responsiveWidth(40),
+		height: responsiveHeight(6),
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	headerIcon: {
+		height: responsiveHeight(2.5),
+		width: responsiveHeight(2.5),
+		padding: responsiveHeight(1)
+	},
+	headerButtonContainer: {
+		marginRight: responsiveHeight(1)
+	},
+	buttonTitle: {
+		fontSize: responsiveHeight(2),
+		fontWeight: 'bold',
+		color: '#fff'
+	},
+	verticalSeparator: {
+		borderColor: '#fff',
+		borderRightWidth: StyleSheet.hairlineWidth * 3
 	}
 });
---
+++ src/screens/Quiz/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { View, ScrollView, TouchableOpacity, Text } from 'react-native';
+import { Paragraph, Title } from 'react-native-paper';
+import styles from './styles';
+import AnswerComponent from './components/Answer';
+const data = [
+	{
+		id: 'answer-1',
+		answer: 'Not at All'
+	},
+	{
+		id: 'answer-2',
+		answer: 'Rarely'
+	},
+	{
+		id: 'answer-3',
+		answer: 'Sometimes'
+	},
+	{
+		id: 'answer-4',
+		answer: 'Often'
+	}
+];
+const QuizScreen = () => {
+	const [answers, setAnswer] = useState(data);
+	const _onPickAnswer = id => {
+		const allAnswers = answers.map(item => {
+			if (item.id !== id) {
+				return { ...item, checked: false };
+			}
+			return { ...item, checked: true };
+		});
+		setAnswer(allAnswers);
+	};
+	return (
+		<ScrollView contentContainerStyle={styles.container}>
+			<View style={styles.header}>
+				<Title style={styles.title}>Question 1/10</Title>
+			</View>
+
+			<Text style={styles.question}>
+				I feel run down and drained of physical or emotional energy.
+			</Text>
+			<View style={styles.answersContainer}>
+				{answers.map(item => (
+					<TouchableOpacity
+						key={item.id}
+						onPress={() => _onPickAnswer(item.id)}
+						style={styles.answerContainer}>
+						<AnswerComponent answer={item.answer} checked={item.checked} />
+					</TouchableOpacity>
+				))}
+			</View>
+			<TouchableOpacity
+				style={styles.nextButton}
+				onPress={() => console.log('NextNextNext')}>
+				<Text style={styles.nextButtonTitle}>Next</Text>
+			</TouchableOpacity>
+		</ScrollView>
+	);
+};
+QuizScreen.navigationOptions = () => ({
+	headerBackTitleVisible: false
+});
+export default QuizScreen;
